b6b7f6bef65edcbd1481d0c9aecfb10ddb6928e5
JayWenStar <1599603313@qq.com>
2022-06-07 10:33:01 +0800

    save

--- sonic-server-controller/src/main/java/org/cloud/sonic/controller/zookeeper/AgentStatusListener.java
+++ sonic-server-controller/src/main/java/org/cloud/sonic/controller/zookeeper/AgentStatusListener.java
@@ -22,6 +22,7 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.cache.CuratorCache;
 import org.apache.curator.framework.recipes.cache.CuratorCacheListener;
 import org.cloud.sonic.common.models.domain.Agents;
+import org.cloud.sonic.common.models.interfaces.AgentStatus;
 import org.cloud.sonic.common.services.AgentsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,14 +39,31 @@ public class AgentStatusListener {
 
     @Bean
     public boolean agentsStatusListener(CuratorFramework curatorFramework, AgentsService agentsService) {
-        CuratorCache curatorCache = CuratorCache.build(curatorFramework, "/sonic-agent");
+        String path = "/sonic-agent";
+        CuratorCache curatorCache = CuratorCache.build(curatorFramework, path);
         curatorCache.listenable().addListener((type, oldData, data) -> {
-            if (CuratorCacheListener.Type.NODE_DELETED != type) {
-                return;
+            if (CuratorCacheListener.Type.NODE_DELETED == type) {
+                String agentJson = new String(oldData.getData());
+                Agents agents = JSON.parseObject(agentJson, Agents.class);
+                agentsService.offLine(agents);
             }
-            String agentJson = new String(oldData.getData());
-            Agents agents = JSON.parseObject(agentJson, Agents.class);
-            agentsService.offLine(agents);
+            if (CuratorCacheListener.Type.NODE_CREATED == type) {
+                if (data.getPath().equals(path)) {
+                    return;
+                }
+                int agentId = Integer.parseInt(data.getPath().split("/")[2]);
+                Agents agents = agentsService.findById(agentId);
+                if (agents == null) {
+                    return;
+                }
+                boolean online = agentsService.checkOnline(agents);
+                if (!online) {
+                    // server -> agent failed
+                    agents.setStatus(AgentStatus.S2AE);
+                    agentsService.updateAgentsByLockVersion(agents);
+                }
+                // online说明agent启动了，设备状态会同步过来，这里不做设备状态检查
+            }
         });
         curatorCache.start();
         log.info("start watch /sonic-agent");
